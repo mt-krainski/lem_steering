c481a68458771dc58bd03e98a2813e2771c78f20
LEM_Raspberry <mateusz@lemrover.co>
2016-07-15 13:43:00 +0000

    Minor updates

--- nodes/joypad_driver.py
+++ nodes/joypad_driver.py
@@ -7,28 +7,48 @@ import numpy as np
 from geometry_msgs.msg import Twist
 import os
 
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
+pygame.init()
+pygame.joystick.init()
+
+from contextlib import contextmanager
+import sys, os
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
 
 class PadDriver:
     def __init__(self, name='PLAYSTATION(R)3 Controller'):
         self.shutdownCounter = 0
         self.Pad = []
+	self.publish = True
+	self.selectPressed=False
+	self.R3Pressed = False
+	self.limitPower = [1.0, 1.0]
 
-        pygame.init()
-        pygame.joystick.init()
         joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
         for joy in joysticks:
             if 'PLAYSTATION(R)3 Controller' in joy.get_name():
                 self.Pad = joy
                 break
 
-
         self.Pad.init()
 
     def sendValues(self):
         pygame.event.get()
-        numAxes = self.Pad.get_numaxes()
-        numButtons = self.Pad.get_numbuttons()
+	with suppress_stdout():
+	    numAxes = self.Pad.get_numaxes()
+	with suppress_stdout():
+            numButtons = self.Pad.get_numbuttons()
         
         Axes = []
         Buttons = []
@@ -40,7 +60,12 @@ class PadDriver:
             Buttons = np.append(Buttons, self.Pad.get_button(i))
         
         linear = 0.7 * (Axes[13] - Axes[12]) + Buttons[14] * 0.3 * (Axes[13] - Axes[12])
-        angular = 0.7 * Axes[0] + Buttons[14] * 0.3 * Axes[0]
+        if abs(Axes[0])<0.2:
+		Axes[0] = 0
+	angular = 0.7 * Axes[0] + Buttons[14] * 0.3 * Axes[0]
+
+	grasperRoll = Axes[2]
+        grasperPitch = Axes[3]
 
 	if Buttons[3]:
 	    self.shutdownCounter += 1
@@ -51,19 +76,48 @@ class PadDriver:
             print "Shutdown command detected!"
             os.system("sudo shutdown -h now")        
 
-        self.ROS_Publish(linear, angular)
+	if Buttons[0]:
+	    if not self.selectPressed:
+   	        if self.publish:
+	            self.publish=False
+	        else:
+	            self.publish=True
+		self.selectPressed=True
+	else:
+	    if self.selectPressed:
+	        self.selectPressed=False
+
+	if Buttons[2]:
+            if not self.R3Pressed:
+                if self.limitPower[0]==1.0:
+                    self.limitPower[0] = 0.3
+		    self.limitPower[1] = 0.6
+                else:
+                    self.limitPower[0]=1.0
+		    self.limitPower[1]=1.0
+                self.R3Pressed=True
+        else:
+            if self.R3Pressed:
+                self.R3Pressed=False
+
+
+	if self.publish:
+	    self.ROS_Publish(self.limitPower[0]*linear, self.limitPower[1]*angular,  grasperRoll, grasperPitch)
     
     def ROS_InitNode(self):
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         rospy.init_node('Pad_driver', anonymous=True)
         self.rate = rospy.Rate(10)
         
-    def ROS_Publish(self, linear, angular):
+    def ROS_Publish(self, linear, angular, grasperRoll, grasperPitch):
         message = Twist()        
         message.linear.x = linear
         message.angular.z = angular
+	message.angular.y = grasperRoll
+        message.angular.x = grasperPitch
         #rospy.loginfo(message)
-        self.pub.publish(message)
+	if self.publish:
+	    self.pub.publish(message)
         
     def ROS_spin(self):
         self.rate.sleep()
